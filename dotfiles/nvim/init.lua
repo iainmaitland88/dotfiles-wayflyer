eef9eea4feadc8fdcd58a1b8eb2af482bdff6710
Iain Maitland <iain.maitland@wayflyer.com>
2023-03-26 12:05:43 +0100

    Add basic neovim config

---
+++ dotfiles/nvim/init.lua
@@ -0,0 +1,57 @@
+-- disable netrw on start
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- tabs & indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- line wrapping
+vim.opt.wrap = false
+
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- cursor line
+vim.opt.cursorline = true
+
+-- appearance
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+-- backspace
+vim.opt.backspace = "indent,eol,start"
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- leader
+vim.g.mapleader = " " 
+vim.g.maplocalleader = " " 
+
+-- keymappings
+keymap = vim.keymap
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "x", '"_x') -- don't copy deleted keys to register
+
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+keymap.set("n", "<leader>to", ":tabnew<CR>")
+keymap.set("n", "<leader>tx", ":tabclose<CR>")
+keymap.set("n", "<leader>tn", ":tabnext<CR>")
+keymap.set("n", "<leader>tp", ":tabprevious<CR>")
